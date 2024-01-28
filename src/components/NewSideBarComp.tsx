8be5713c7b1464f6c37e8207f10c75d1eea98b57
Samit_08 <almustansir465@gmail.com>
2024-01-28 21:18:07 +0600

    testing...

---
+++ src/components/NewSideBarComp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SideBarComp = () => {
+  return (
+    <div>
+      <section></section>
+      <section></section>
+    </div>
+  );
+};
+
+export default SideBarComp;
