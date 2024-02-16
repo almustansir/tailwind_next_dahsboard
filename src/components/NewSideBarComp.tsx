293adacabceb2b54e91e05cd5b7494b42eefadf0
Samit_08 <almustansir465@gmail.com>
2024-02-16 18:31:05 +0600

    added product page

--- src/components/NewSideBarComp.tsx
+++ src/components/NewSideBarComp.tsx
@@ -105,6 +105,14 @@ const SideBarComp = () => {
             </li>
             <li>
               <a
+                href="./products"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <span className="ms-3">Product ID</span>
+              </a>
+            </li>
+            <li>
+              <a
                 href="./account"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBarComp from "../components/NewSideBarComp";
+import CustomerModalComp from "../components/CustomerModalComp";
 
 const Customers = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
   return (
     <div className="p-4 md:ml-64">
       <SideBarComp />
       <div>
         <div className=" flex justify-between">
           <h4 className=" text-3xl">Customers List:</h4>{" "}
-          <button className="text-blue-700 md:w-64 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          <button
+            onClick={toggleModal}
+            className="text-blue-700 md:w-64 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
             Add New Entry
           </button>
         </div>
+        {/* modal */}
+        <div>{modal && <CustomerModalComp />}</div>
+
         <div className="flex flex-col items-center justify-center mb-4 rounded bg-gray-50 border-2 border-gray-200">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50">
